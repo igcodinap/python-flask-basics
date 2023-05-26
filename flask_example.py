dfaa06d39fa4e585ff5d74c2ab72527c4aab3759
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-26 19:27:20 -0400

    example

--- flask_example.py
+++ flask_example.py
@@ -1,9 +1,43 @@
 from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
 # Flask(flask_example)
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost:3306/itemsDB'
+
+db = SQLAlchemy(app)
+
 items_db = {}
 #{'1': { id: 1, name: xxxxx}, '2': { id: 2, name: xxxxx}, '3': { id: 3, name: xxxxx}}
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+# @app.route('/items', methods=['POST'])
+# def create_item():
+#     print(request.json)
+#     item = {
+#         'id': str(len(items_db) + 1 ),
+#         'name': request.json['name'],
+#     }
+#     items_db[item['id']] = item
+#     return jsonify(item), 203
+
+@app.route('/items', methods=['POST'])
+def create_item():
+    newItem = Item(name=request.json['name'])
+    db.session.add(newItem)
+    # db.session.add(newUser)
+    # db.session.add(newWallet)
+    db.session.commit()
+    return jsonify(newItem.serialize()), 201
 
 @app.route('/', methods=['GET'])
 def hello_world():
@@ -17,15 +51,6 @@ def hello_world():
 def get_items():
     return jsonify(list(items_db.values()))
 
-@app.route('/items', methods=['POST'])
-def create_item():
-    print(request.json)
-    item = {
-        'id': str(len(items_db) + 1 ),
-        'name': request.json['name'],
-    }
-    items_db[item['id']] = item
-    return jsonify(item), 203
 
 @app.route('/items/<string:item_id>', methods=['GET'])
 def get_item(item_id):
