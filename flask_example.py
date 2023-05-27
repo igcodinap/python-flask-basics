e93ac63b217700e3b510a91db81eb216a9eac176
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-26 20:15:49 -0400

    sqlalqchemy

--- flask_example.py
+++ flask_example.py
@@ -11,14 +11,43 @@ items_db = {}
 #{'1': { id: 1, name: xxxxx}, '2': { id: 2, name: xxxxx}, '3': { id: 3, name: xxxxx}}
 
 class Item(db.Model):
+    __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30))
-
-    def serialize(self):
+    def serialize1(self):
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
         }
+
+    def serialize2(self): 
+        return {
+            'name': self.name,
+        }
+
+
+@app.route('/items', methods=['POST'])
+def create_item():
+    newItem = Item(name=request.json['name'])
+
+    db.session.add(newItem)
+    db.session.commit()
+    return jsonify(newItem.serialize()), 201
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all() # SELECT * FROM items
+    return jsonify([item.serialize() for item in items])
+
+@app.route('/items/<string:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = Item.query.get(item_id) # SELECT * FROM items WHERE id=item_id
+    if item is None:
+        abort(404)
+    return jsonify(item.serialize())
+
+
+############### old CRUD - soon gonna be eliminated #####################
 
 # @app.route('/items', methods=['POST'])
 # def create_item():
@@ -30,33 +59,15 @@ class Item(db.Model):
 #     items_db[item['id']] = item
 #     return jsonify(item), 203
 
-@app.route('/items', methods=['POST'])
-def create_item():
-    newItem = Item(name=request.json['name'])
-    db.session.add(newItem)
-    # db.session.add(newUser)
-    # db.session.add(newWallet)
-    db.session.commit()
-    return jsonify(newItem.serialize()), 201
+# @app.route('/items', methods=['GET'])
+# def get_items():
+#     return jsonify(list(items_db.values()))
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    greeting = 'hola mundo'
-    return greeting
-
-# CRUD
-
-#ruta para GET
-@app.route('/items', methods=['GET'])
-def get_items():
-    return jsonify(list(items_db.values()))
-
-
-@app.route('/items/<string:item_id>', methods=['GET'])
-def get_item(item_id):
-    if item_id not in items_db:
-        abort(404)
-    return jsonify(items_db[item_id])
+#@app.route('/items/<string:item_id>', methods=['GET'])
+# def get_item(item_id):
+#     if item_id not in items_db:
+#         abort(404)
+#     return jsonify(items_db[item_id])
 
 # PUT
 @app.route('/items/<string:item_id>', methods=['PUT'])
